3fdb9d00900ef046d56586e6f69a83ca7fa1468e
Tiago Aguiar <tyago_tka48@hotmail.com>
2020-03-31 20:42:00 -0300

    Backend da aplicaçao

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,20 @@
 const express = require('express')
-const crypto = require('crypto')
-const connection = require('./database/connection')
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
 const routes = express.Router()
 
-routes.get('/users/:id', (request, response) => {
-  // const params = request.query;
-  const params = request.params;
+routes.post('/sessions', SessionController.create)
 
-  console.log(params)
-  return response.json({nome: 'Tiago', email: 'tigo@email.com.br'})
-})
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create)
 
-routes.post('/ongs', async (request, response) => {
-  const { name, email, whatsapp, city, uf } = request.body
-  const id = crypto.randomBytes(4).toString('HEX')
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete)
 
-  await connection('ongs').insert({
-    id,name,email,whatsapp,city,uf
-  })
-
-  return response.json({ id })
-})
+routes.get('/profile', ProfileController.index)
 
 module.exports = routes
 
